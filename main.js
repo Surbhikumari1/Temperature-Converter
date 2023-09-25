7ea4d2c3a7bbbe3b4a320e9725eb229441eab15e
Surbhikumari1 <146000144+Surbhikumari1@users.noreply.github.com>
2023-09-25 21:55:54 +0530

    Add files via upload
    add html,css and js file

---
+++ main.js
@@ -0,0 +1,34 @@
+function convertTemperature() {
+    const inputTemp = parseFloat(document.getElementById("inputTemp").value);
+    const fromUnit = document.getElementById("fromUnit").value;
+    const toUnit = document.getElementById("toUnit").value;
+    let result = 0;
+
+    if (fromUnit === "celsius") {
+        if (toUnit === "fahrenheit") {
+            result = (inputTemp * 9/5) + 32;
+        } else if (toUnit === "kelvin") {
+            result = inputTemp + 273.15;
+        } else {
+            result = inputTemp;
+        }
+    } else if (fromUnit === "fahrenheit") {
+        if (toUnit === "celsius") {
+            result = (inputTemp - 32) * 5/9;
+        } else if (toUnit === "kelvin") {
+            result = (inputTemp - 32) * 5/9 + 273.15;
+        } else {
+            result = inputTemp;
+        }
+    } else if (fromUnit === "kelvin") {
+        if (toUnit === "celsius") {
+            result = inputTemp - 273.15;
+        } else if (toUnit === "fahrenheit") {
+            result = (inputTemp - 273.15) * 9/5 + 32;
+        } else {
+            result = inputTemp;
+        }
+    }
+
+    document.getElementById("result").textContent = `${inputTemp} ${fromUnit} is equal to ${result.toFixed(2)} ${toUnit}`;
+}
